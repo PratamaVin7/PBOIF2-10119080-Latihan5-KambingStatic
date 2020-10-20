ed73cfdc999a598406bf6dad5f64ec023c157737
unknown <kevinpratamaoke@gmail.com>
2020-10-20 15:17:55 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan5/kambingglobal/PBOIF210119080Latihan5KambingGlobal.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan5.kambingglobal;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210119080Latihan5KambingGlobal {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Variabel jumlah kambing menjadi variabel instance
+    int jumlahKambing = 88;
+    
+    //Method untuk menampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " +
+                            jumlahKambing);
+    }
+    public static void main(String[] args) {
+        PBOIF210119080Latihan5KambingGlobal kambingSusu = new 
+        PBOIF210119080Latihan5KambingGlobal();
+        
+        //Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        //Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        //Menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
